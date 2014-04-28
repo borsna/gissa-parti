75beffd18e9ac26b9b64029290b495211636691c
Olof Olsson <borsna@gmail.com>
2014-04-28 14:15:56 +0200

    fix bug, removes attribute

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
             textVisible: true,
             textonly: false
         });
-        $("#score li").removeClass();
+        $("#score li").removeAttr('class');
         quiz.loadNewSentences(function() {
             $.mobile.loading("hide");
             loadSentence();
